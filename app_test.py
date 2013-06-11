c51ffba4ca7a8b93165190e5fb73887cba7ef182
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2013-06-11 15:46:51 +0200

    clean up

--- app_test.py
+++ app_test.py
@@ -18,9 +18,7 @@ class CloudTestCase(unittest.TestCase):
 
   def test_clouds(self):
     tester = app.test_client(self)
-
     response = tester.get('/clouds.json', content_type='application/json')
-
     self.assertEqual(response.status_code, 200)
     self.assertEqual(response.data, json.dumps(['Altocumulus', 'Altostratus',
       'Cumulonimbus', 'Nimbostratus']))
